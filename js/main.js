b549e5b1166add7eb7597a207466f9bfdd905223
System Administrator <root@Jamess-iMac.local>
2018-02-25 10:57:58 -0800

    update

--- js/main.js
+++ js/main.js
@@ -1,68 +1,9 @@
-// Typing functionality in homepage hero
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.09em solid #aaa }";
-  document.body.appendChild(css);
-};
-
-
 // Add onscroll animations
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
      //>=, not <=
-    if (scroll >= 100) {
+    if (scroll >= 80) {
         //clearHeader, not clearheader - caps H
         $(".project-1 img").addClass("project-animate");
     }
@@ -71,7 +12,7 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
      //>=, not <=
-    if (scroll >= 800) {
+    if (scroll >= 650) {
         //clearHeader, not clearheader - caps H
         $(".project-2 img").addClass("project-animate");
     }
@@ -80,7 +21,7 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
      //>=, not <=
-    if (scroll >= 1500) {
+    if (scroll >= 1200) {
         //clearHeader, not clearheader - caps H
         $(".progress-bar").addClass("grow");
     }
