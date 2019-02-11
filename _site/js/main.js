7f797c2691c2e29ddc54b182ed1fa4b2a934f544
jadoran <jadoran@ebay.com>
2019-02-10 20:33:43 -0800

    venetian blind update

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,6 +1,7 @@
-
-
 jQuery(document).ready(function($) {
+
+    var w = window.innerWidth;
+    var arrow = w / 6 * 1 - 20
     
 	TweenLite.set('.text-js, .descriptor, .hero-arrow, .contact, #home-line, .line-element', {x:'-101%'})
 
@@ -8,11 +9,12 @@ jQuery(document).ready(function($) {
     .to('.text-js',1,{x:'0%', ease: Power2.easeOut})
     .to('.descriptor,.hero-arrow',1,{x:'0%', opacity:1, ease: Power2.easeOut})
     .to('.hero-arrow',1,{x:'0%', opacity: 1, ease: Power2.easeOut},'-=0')
-    .to('.arrow-parent',2,{right:'15.4%', delay: .8, ease: Power2.easeOut},'2')
-    .staggerFrom('#work, .arrow-lines',1,{scaleY: 0, transformOrigin: "top center", ease: Power2.easeOut})
-    .staggerTo('.contact',1,{x:'0%', ease: Power2.easeOut},'-=2')
-    // .to('.line-element',1,{scaleX: 1000, backgroundColor:'white'})
-
+    .to('.arrow-parent',1,{delay:.8, right:arrow, ease: Power2.easeOut},'1.5')
+    .staggerFrom('#work, .arrow-lines',2,{scaleY: 0, transformOrigin: "top center", ease: Power2.easeOut})
+    .to('.contact',1,{x:'0%', ease: Power2.easeOut},'-=.3')
+    .fromTo("#envelope",1 ,{drawSVG:1}, {drawSVG:"100%"})
+    .staggerTo(".balloon", 5, {repeat:-1,x:'-=5px', delay:0,rotation: 1, y:'-=20px',yoyo: true,ease: Power2.easeInOut},'-=2.5')
+    .staggerTo(".balloon", 2, {repeat:-1,rotation: 2,delay:0, ease: Power2.easeInOut},'-=2.5')
 
 	//Scroll Starts
     
@@ -21,8 +23,11 @@ jQuery(document).ready(function($) {
     
     var tlMainScroll = new TimelineMax()
     .add([
-        TweenMax.to(lines, .1, {backgroundColor:'white'}),
-		TweenMax.to(lines, 1, {scaleX: 1000, backgroundColor:'white', transformOrigin: "left"}),
+        TweenMax.to(lines, .001, {backgroundColor:'white', autoAlpha:1}),
+        TweenMax.to('.descriptor span, .contact', .001, {color:'white', borderBottomColor:'white'}),
+        TweenMax.to(lines, 500, {scaleX: 1000, transformOrigin: "left"}), 
+        TweenMax.to('.bg, .title-main', 100, {autoAlpha:0, delay:4}),
+        TweenMax.to(lines, 200, {autoAlpha:0, delay:4}),       
     ]);
 
     
@@ -30,27 +35,30 @@ jQuery(document).ready(function($) {
     var tweenHome = new ScrollMagic.Scene({
         triggerElement: '.pin-scene',
         triggerHook:0,
-        duration: '130%'})
+        duration: '500%'})
     
     .setTween(tlMainScroll)
     .setPin('.pin-scene')
     .addTo(controller);
 
 
+
+
 //Scroll Out 
 
 var tlMainScrollOut = new TimelineMax()
     .add([
-        TweenMax.to(bgImage, 8, {autoAlpha:0}),
-        TweenMax.from('.section-2', 4, {autoAlpha:0}),
-		TweenMax.to(spaceman, 8, {autoAlpha:0}),
-		TweenMax.to(stars, 2, {autoAlpha:0})
+        TweenMax.to('.title-main, .contact', 0.1, {autoAlpha:0}),
+        TweenMax.from('.section2', 10, {autoAlpha:0}),
+        TweenMax.from('.balloon', 1500, {scale:0, ease: Power4.easeOut}),
+        TweenMax.from('.heading', 500, {y:'100', delay:20,autoAlpha:0,ease: Power4.easeOut}),
+        TweenMax.from('.text-content', 500, {y:'100', delay:20,autoAlpha:0,ease: Power4.easeOut}),
     ]);
 
 var section2 = new ScrollMagic.Scene ({
     triggerElement: '.section2',
     triggerHook: 0,
-    duration: '40%'})
+    duration: '80%'})
 .setTween(tlMainScrollOut)
 .setPin('.section2')
 .addTo(controller);
@@ -58,27 +66,3 @@ var section2 = new ScrollMagic.Scene ({
  });
 
 
-//once js runs un-hide the svg
-TweenLite.set(".contact svg", {visibility:"visible"});
-//hide the stroke
-TweenLite.set("rect", {drawSVG:0})
-
-
-//create a tween in a paused state
-var tween = TweenLite.to("rect", 0.5, {drawSVG:true, paused:true});
-
-
-
-
-// play/reverse on over/out
-$("rect").hover(over, out);
-
-
-function out() {
-  tween.reverse();
-}
-
-
-function over() {
-  tween.play();
-}
