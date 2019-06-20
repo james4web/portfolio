28b30a4e0893bc0cc8667a566e9cf5b73a73b834
jadoran <jadoran@ebay.com>
2019-06-20 11:20:39 -0700

    ebay page

--- _site/js/main.js
+++ _site/js/main.js
@@ -64,4 +64,24 @@ var section2 = new ScrollMagic.Scene ({
  
  });
 
+//  Tabs
+$(document).ready(function() {    
+
+    $('#tabs li a:not(:first)').addClass('inactive');
+    $('.tab-container').hide();
+    $('.tab-container:first').show();
+        
+    $('#tabs li a').click(function(){
+        var t = $(this).attr('id');
+      if($(this).hasClass('inactive')){ //this is the start of our condition 
+        $('#tabs li a').addClass('inactive');           
+        $(this).removeClass('inactive');
+        
+        $('.tab-container').hide();
+        $('#'+ t + 'C').fadeIn('slow');
+     }
+    });
+});
+    
+
 
